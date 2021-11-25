6948f85213ad292176f4ded433cb4770afa2c4ba
PhilipJanson <ph8671ja-s@student.lu.se>
2021-11-25 15:29:34 +0100

    Finished

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -3,47 +3,63 @@ import requests
 import argparse
 from time import sleep
 
-#Write you own function that moves the drone from one place to another 
-#the function returns the drone's current location while moving
-#====================================================================================================
-def moveDrone(current_coords, from_coords, to_coords, index):
+
+def moveDrone(current_coords, next_coords, index):
     
-    delta_long = (from_coords[0] - current_coords[0])
-    delta_lat = (from_coords[1] - current_coords[1])
+    # Calculate the difference in distance for both x and y
+    # Calculate the total distance between start and end
+    delta_long = (next_coords[0] - current_coords[0])
+    delta_lat = (next_coords[1] - current_coords[1])
     dist = math.sqrt(delta_long ** 2 + delta_lat ** 2)
 
     sleep(0.02)
     
     return (current_coords[0] + index * ((delta_long / dist) / 10000), current_coords[1] + index * ((delta_lat / dist) / 10000))
-#====================================================================================================
 
 
 def run(current_coords, from_coords, to_coords, SERVER_URL):
-    # Compmelete the while loop:
-    # 1. Change the loop condition so that it stops sending location to the data base when the drone arrives the to_address
-    # 2. Plan a path with your own function, so that the drone moves from [current_address] to [from_address], and the from [from_address] to [to_address]. 
-    # 3. While moving, the drone keeps sending it's location to the database.
-    #====================================================================================================
-    index = 0
-    dist_x = abs(current_coords[0] - from_coords[0])
-    dist_y = abs(current_coords[1] - from_coords[1])
     
+    # Setup
+    # index starts at 0
+    # Set the next destiantion to the first adress
+    # Calculate the distance to the end location
+    index = 0
+    next_coords = from_coords
+    dist_x = abs(current_coords[0] - next_coords[0])
+    dist_y = abs(current_coords[1] - next_coords[1])
+    
+    # Keep looping if the distance of both the x and y are not 0.001 from the end.
     while (dist_x > 0.001 or dist_y > 0.001):
+        
+        # Add 1 to index and move the drone
         index += 1
-        print(dist_x)
-        print(dist_y)
+        drone_coords = moveDrone(current_coords, next_coords, index)
         
-        drone_coords = moveDrone(current_coords, from_coords, to_coords, index)
+        # Calculate the new distance from the end coordinates
+        # after the drone has moved.
+        dist_x = abs(drone_coords[0] - next_coords[0])
+        dist_y = abs(drone_coords[1] - next_coords[1])
         
-        dist_x = abs(drone_coords[0] - from_coords[0])
-        dist_y = abs(drone_coords[1] - from_coords[1])
-        
+        # Check if the drone has reached its destination, if it has:
+        # Set the current_coords to the actual location.
+        # Set the next destiantion to the second adress.
+        # Calculate the new distance after changing the start and end position.
+        if not (dist_x > 0.001 or dist_y > 0.001):
+            current_coords = drone_coords
+            next_coords = to_coords
+            index = 0
+            dist_x = abs(drone_coords[0] - next_coords[0])
+            dist_y = abs(drone_coords[1] - next_coords[1])
+            print("Destination reached")
+            sleep(0.5)
+            
         with requests.Session() as session:
             drone_location = {'longitude': drone_coords[0],
                               'latitude': drone_coords[1]
                         }
             resp = session.post(SERVER_URL, json=drone_location)
-  #====================================================================================================
+    
+    print("End reached")
 
    
 if __name__ == "__main__":
